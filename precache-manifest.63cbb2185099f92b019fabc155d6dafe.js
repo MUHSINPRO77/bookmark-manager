21f86ec460f7865af0ee66cbdaf4e1258e5ea0e1
Your Name <you@example.com>
2020-10-04 16:18:59 +0530

    Updates

---
+++ precache-manifest.63cbb2185099f92b019fabc155d6dafe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf9cb6a29d07892dda9f7493616f4425",
+    "url": "/MUHSINPRO77/bookmark-manager.git/index.html"
+  },
+  {
+    "revision": "cd701708860667f8e7f4",
+    "url": "/MUHSINPRO77/bookmark-manager.git/static/css/main.791376d3.chunk.css"
+  },
+  {
+    "revision": "9a9e7d6272cab1b55993",
+    "url": "/MUHSINPRO77/bookmark-manager.git/static/js/2.e657d8b4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MUHSINPRO77/bookmark-manager.git/static/js/2.e657d8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd701708860667f8e7f4",
+    "url": "/MUHSINPRO77/bookmark-manager.git/static/js/main.b726f8ac.chunk.js"
+  },
+  {
+    "revision": "094ae2b77cde6c0d8277",
+    "url": "/MUHSINPRO77/bookmark-manager.git/static/js/runtime-main.a62c82bd.js"
+  },
+  {
+    "revision": "5b1f481cf48d9559db329a8b0458730c",
+    "url": "/MUHSINPRO77/bookmark-manager.git/static/media/github.5b1f481c.jpg"
+  },
+  {
+    "revision": "e45ebd5258d77392cb78ceb7b3133eeb",
+    "url": "/MUHSINPRO77/bookmark-manager.git/static/media/logo.e45ebd52.png"
+  },
+  {
+    "revision": "427b8bc29df2dae700937053281fcaa7",
+    "url": "/MUHSINPRO77/bookmark-manager.git/static/media/navlogo.427b8bc2.png"
+  },
+  {
+    "revision": "5e39cdaae9d6c9ada5c2de4b2cf416ae",
+    "url": "/MUHSINPRO77/bookmark-manager.git/static/media/white.5e39cdaa.jpg"
+  }
+]);
